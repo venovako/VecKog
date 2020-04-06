a7bf9675a6e13c42a0970186f8557955b9c2bc88
Vedran Novakovic <venovako@venovako.eu>
2020-04-06 21:09:32 +0200

    Add the makefile includes.

---
+++ src/x200.mk
@@ -0,0 +1,28 @@
+SHELL=/bin/bash
+ARCH=$(shell uname)
+ifdef NDEBUG
+DEBUG=
+else # DEBUG
+DEBUG=g
+endif # ?NDEBUG
+RM=rm -rfv
+AR=xiar
+ARFLAGS=-qnoipo -lib rsv
+CC=icc -std=c18
+CPUFLAGS=-DUSE_INTEL -DUSE_X200 -DUSE_TSC -fPIC -fexceptions -fno-omit-frame-pointer -qopenmp -rdynamic
+ifdef PROFILE
+CPUFLAGS += -DVN_PROFILE=$(PROFILE) -fno-inline -finstrument-functions
+endif # PROFILE
+C18FLAGS=$(CPUFLAGS)
+ifdef NDEBUG
+OPTFLAGS=-O$(NDEBUG) -xHost
+DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3 -diag-disable=1572,2547,10397
+FPUFLAGS=-fma -fp-model source -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high -fimf-use-svml=true
+else # DEBUG
+OPTFLAGS=-O0 -xHost
+DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug parallel -debug pubnames -traceback -check=stack,uninit -w3 -diag-disable=1572,2547,10397
+FPUFLAGS=-fp-model strict -fp-stack-check -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high
+endif # ?NDEBUG
+LIBFLAGS=-D_GNU_SOURCE -I. -I.. -I../../../JACSD/vn -DUSE_MKL -I${MKLROOT}/include/intel64/lp64 -I${MKLROOT}/include
+LDFLAGS=-L.. -L../../../JACSD -lvn$(PROFILE)$(DEBUG) -L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -lmemkind
+CFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(C18FLAGS) $(FPUFLAGS)
---
+++ src/x64.mk
@@ -0,0 +1,38 @@
+SHELL=/bin/bash
+ARCH=$(shell uname)
+ifdef NDEBUG
+DEBUG=
+else # DEBUG
+DEBUG=g
+endif # ?NDEBUG
+RM=rm -rfv
+AR=xiar
+ARFLAGS=-qnoipo -lib rsv
+CC=icc -std=c18
+CPUFLAGS=-DUSE_INTEL -DUSE_X64 -DUSE_TSC -fPIC -fexceptions -fno-omit-frame-pointer -qopenmp -rdynamic
+ifdef PROFILE
+CPUFLAGS += -DVN_PROFILE=$(PROFILE) -fno-inline -finstrument-functions
+endif # PROFILE
+C18FLAGS=$(CPUFLAGS)
+ifdef NDEBUG
+OPTFLAGS=-O$(NDEBUG) -xHost
+DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3 -diag-disable=1572,2547,10397
+FPUFLAGS=-fma -fp-model source -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high -fimf-use-svml=true
+else # DEBUG
+OPTFLAGS=-O0 -xHost
+DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -traceback -check=stack,uninit -w3 -diag-disable=1572,2547,10397
+ifneq ($(ARCH),Darwin) # Linux
+DBGFLAGS += -debug parallel
+endif # ?Linux
+FPUFLAGS=-fp-model strict -fp-stack-check -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high
+endif # ?NDEBUG
+LIBFLAGS=-I. -I.. -I../../../JACSD/vn -DUSE_MKL -I${MKLROOT}/include/intel64/lp64 -I${MKLROOT}/include
+LDFLAGS=-L.. -L../../../JACSD -lvn$(PROFILE)$(DEBUG)
+ifeq ($(ARCH),Darwin)
+LDFLAGS += -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
+else # Linux
+LIBFLAGS += -D_GNU_SOURCE
+LDFLAGS += -L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
+endif # ?Darwin
+LDFLAGS += -lpthread -lm -ldl
+CFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(C18FLAGS) $(FPUFLAGS)
