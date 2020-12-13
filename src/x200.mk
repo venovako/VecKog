8a67870f2ee55fcb063075a0ceb191a3bbc98f0c
Vedran Novakovic <venovako@venovako.eu>
2020-12-13 20:13:02 +0100

    Update the makefiles.

--- src/x200.mk
+++ src/x200.mk
@@ -14,15 +14,15 @@ ifdef TEST
 CPUFLAGS += -DTEST=$(TEST)
 endif # TEST
 C18FLAGS=$(CPUFLAGS)
-FPUFLAGS=-fp-model source -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high -fimf-use-svml=true
+FPUFLAGS=-fp-model precise -fprotect-parens -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-use-svml=true
 ifdef NDEBUG
-OPTFLAGS=-O$(NDEBUG) -xHost -qopt-zmm-usage=high
+OPTFLAGS=-O$(NDEBUG) -xHost -qopt-multi-version-aggressive -qopt-zmm-usage=high
 DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3
 else # DEBUG
-OPTFLAGS=-O0 -xHost -qopt-zmm-usage=high
+OPTFLAGS=-O0 -xHost -qopt-multi-version-aggressive -qopt-zmm-usage=high
 DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug parallel -debug pubnames -traceback -check=stack,uninit -w3
 FPUFLAGS += -fp-stack-check
 endif # ?NDEBUG
-LIBFLAGS=-D_GNU_SOURCE -I. -DUSE_MKL -I${MKLROOT}/include/intel64/lp64 -I${MKLROOT}/include
+LIBFLAGS=-static-libgcc -D_GNU_SOURCE -I. -DUSE_MKL -I${MKLROOT}/include/intel64/lp64 -I${MKLROOT}/include
 LDFLAGS=-L. -lveckog$(TEST)$(DEBUG) -L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -lmemkind
 CFLAGS=$(OPTFLAGS) $(DBGFLAGS) $(LIBFLAGS) $(C18FLAGS) $(FPUFLAGS)
--- src/x64.mk
+++ src/x64.mk
@@ -14,12 +14,12 @@ ifdef TEST
 CPUFLAGS += -DTEST=$(TEST)
 endif # TEST
 C18FLAGS=$(CPUFLAGS)
-FPUFLAGS=-fp-model source -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high -fimf-use-svml=true
+FPUFLAGS=-fp-model precise -fprotect-parens -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-use-svml=true
 ifdef NDEBUG
-OPTFLAGS=-O$(NDEBUG) -xHost -qopt-zmm-usage=high
+OPTFLAGS=-O$(NDEBUG) -xHost -qopt-multi-version-aggressive
 DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3
 else # DEBUG
-OPTFLAGS=-O0 -xHost -qopt-zmm-usage=high
+OPTFLAGS=-O0 -xHost -qopt-multi-version-aggressive
 DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -traceback -check=stack,uninit -w3
 ifneq ($(ARCH),Darwin) # Linux
 DBGFLAGS += -debug parallel
@@ -31,7 +31,7 @@ LDFLAGS=-L. -lveckog$(TEST)$(DEBUG)
 ifeq ($(ARCH),Darwin)
 LDFLAGS += -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
 else # Linux
-LIBFLAGS += -D_GNU_SOURCE
+LIBFLAGS += -static-libgcc -D_GNU_SOURCE
 LDFLAGS += -L${MKLROOT}/lib/intel64 -Wl,-rpath=${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
 endif # ?Darwin
 LDFLAGS += -lpthread -lm -ldl
