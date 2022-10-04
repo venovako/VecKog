9d445006ed506312cdabd75b7fdfc7488f8df1ee
Vedran Novakovic <venovako@venovako.eu>
2022-10-04 04:20:50 +0200

    a few updates

--- src/x64.mk
+++ src/x64.mk
@@ -17,10 +17,10 @@ C18FLAGS=$(CPUFLAGS)
 FPUFLAGS=-fp-model precise -fprotect-parens -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-use-svml=true
 ifdef NDEBUG
 OPTFLAGS=-O$(NDEBUG) -xHost -qopt-multi-version-aggressive -vec-threshold0
-DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3 -diag-disable=10397
+DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -w3 -diag-disable=10397,10441
 else # DEBUG
 OPTFLAGS=-O0 -xHost -qopt-multi-version-aggressive
-DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -traceback -check=stack,uninit -w3 -diag-disable=10397
+DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -traceback -check=stack,uninit -w3 -diag-disable=10397,10441
 ifneq ($(ARCH),Darwin) # Linux
 DBGFLAGS += -debug parallel
 endif # ?Linux
