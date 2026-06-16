f8a8898b07259980ba620c28c9ffde4ad8cc3c61
Vedran Novakovic <venovako@venovako.eu>
2026-06-16 03:55:24 +0200

    further updates

--- src/build_icx.sh
+++ src/build_icx.sh
@@ -3,14 +3,14 @@
 if [ -z "${TGT}" ]
 then
 	cd ../../libpvn/src
-	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM OPENMP=0 clean
-	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM OPENMP=0 -j all
+	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 clean
+	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j all
 	cd ../../VecKog/src
 	make clean all
 else
 	cd ../../libpvn/src
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM OPENMP=0 clean
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM OPENMP=0 -j all
+	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 clean
+	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j all
 	cd ../../VecKog/src
 	make clean all
 fi
